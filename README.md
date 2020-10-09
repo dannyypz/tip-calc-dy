c815aa4f3957962c0de965e2c765430d49ebbd75
Daniel_Yepez <dannyypz@gmail.com>
2020-10-09 15:37:41 -0400

    This is my commit for my first calculator

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tip calculator
+
+Our tip calculator! Runs on node.js.
+
+To run this, type `node tip-calc-dy.js`.
---
+++ tip-calc-dy.js
@@ -0,0 +1,36 @@
+const readlineSync = require("readline-sync");
+
+// Get the total amount of the bill. Wait for user's response.
+const totalAmount = readlineSync.questionFloat(
+  "What's the total amount of the bill: $"
+);
+
+// Get the party size from the user
+const totalPartySize = readlineSync.questionInt(
+  "How many people are you in your party? "
+);
+
+// Get the % of the tip from the user
+const tipPercentage = readlineSync.questionFloat(
+  "What % would you like to tip? "
+);
+
+// Repeat all of those things back to the user.
+console.log(`The total amount of your bill is $${totalAmount.toFixed(2)}`);
+console.log(`The total party size is ${totalPartySize}`);
+console.log(`With a tip of ${tipPercentage}%`);
+
+// The bill tip is the (totalAmount * tipPercentage divided by 100)
+let percentage = tipPercentage / 100;
+let totalAmountToTip = totalAmount * percentage;
+
+// Total bill is the bill plus the tip
+let billWithTip = totalAmount + totalAmountToTip;
+
+// Get the total amount of the bill divided by the # of people
+let totalPerPerson = billWithTip / totalPartySize;
+
+// Calculate how much everyone is paying
+console.log(`Thhe tip should be $${totalAmountToTip.toFixed(2)}`);
+console.log(`The Toals Check would be $${billWithTip.toFixed(2)}`);
+console.log(`The total tip per person${totalPerPerson.toFixed(2)}`);
